e1e8c5983721bebc1a07d36b7ef72f9b7dfe1bc8
Alex <alex3521@hotmail.fr>
2014-03-08 18:01:35 -0500

    Commit Test

--- Assets/Scripts/YunittoWiggle.cs
+++ Assets/Scripts/YunittoWiggle.cs
@@ -94,7 +94,7 @@ public class YunittoWiggle : MonoBehaviour {
 	}
 
 	private void Move() {
-		// Biaiser les limites de mouvement pour que le yunitto ne s'éloigne pas trop du bunc
+		// Biaiser les limites de mouvement pour que le yunitto ne s'éloigne pas trop du bunch
 		float interest = ((-transform.localPosition.x - (0.6f*unitRange))/leashLength) * wiggleSpeed; //ajouté le unit range pour que les ranges soient plus en arriere.
 		float movement = Random.Range(interest-wiggleSpeed, interest+wiggleSpeed) * Time.deltaTime;
 		transform.localPosition = new Vector3(transform.localPosition.x + movement, 
