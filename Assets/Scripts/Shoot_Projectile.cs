2254c840aaa46e9c49b7bb42c0d7f9e7a3b17a35
Marc <marc1999_8@hotmail.com>
2014-03-08 09:19:25 -0500

    modif Shoot_Projectile(pas vraiment)

--- Assets/Scripts/Shoot_Projectile.cs
+++ Assets/Scripts/Shoot_Projectile.cs
@@ -7,7 +7,7 @@ public class Shoot_Projectile : MonoBehaviour {
 	public int direction=1; 					//Direction du tir, elle ÉGALE A 1 POUR DES TESTS SEULEMENT
 	public float _distance; 					//Distance entre deux unités
 	public float _angleprojectile; 				//Angle de la de la vitesse du projectile
-	public float _angletir=45.0f;						//Angle du  tir du projectile
+	public float _angletir=45.0f;				//Angle du  tir du projectile
 	public float _xspeed; 						//Variable vitesse verticale du projectile
 	public float _yspeed; 						//Variable vitesse horizontale du projectile
 	public float _speed; 						//Variable vitesse du projectile
@@ -53,7 +53,7 @@ public class Shoot_Projectile : MonoBehaviour {
 				_angleprojectile+=360; 
 			}*/
 		//transform.eulerAngles = new Vector3(0.0f, 0.0f, _angleprojectile);
-		_yspeed  -=  _speedreduction*Time.deltaTime;
+		_yspeed  -=  _speedreduction*Time.deltaTime; //La vitesse en y du projectile est soumise a la force de gravité de _speedreduction par seconde
 		//_speed = Mathf.Pow((_xspeed * _xspeed + _yspeed * _yspeed), 0.5f); //USELESS
 
 	}
